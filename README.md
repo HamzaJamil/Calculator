91c4449615a48aef3e9b5bf9d18ebd530f9a83f7
Hamza Jamil <hamzajamil.official@gmail.com>
2023-04-04 22:51:46 +0500

    Calculator npx command generate

---
+++ README.md
@@ -0,0 +1,27 @@
+
+# Command Line Calculator
+
+Simple command line calculator using TypeScipt, Node.js, chalk and Inquirer. It asks for operator then takes two number using prompt from user, then operates on them and shows result.
+
+
+
+
+
+## Run Locally
+
+To run this project, open command line and run the following command
+
+```bash
+  npx calculator-by-hamza
+```
+
+
+## Author:
+
+- [@hamzajamil](https://www.github.com/hamzajamil)
+
+
+## Lessons Learned
+
+This is an assignment of Q1 - Web3.0 and Metaverse Course of PIAIC. Task of this exercise is to learn prompt for taking user inputs using inquirer and chalk package, and publishing calculator package on npm registry, also push this project on github repository.
+
---
+++ calculator.ts
@@ -0,0 +1,71 @@
+#! /usr/bin/env node
+
+// Import Packages
+import chalk from "chalk";
+import inquirer from "inquirer";
+import figlet from "figlet";
+
+console.log(figlet.textSync('Calculator', {
+    font: 'Standard',
+    horizontalLayout: 'fitted',
+    verticalLayout: 'default',
+    width: 80,
+    whitespaceBreak: true
+}));
+console.log(chalk.bold.bgCyan('\t Author: Hamza Jamil ', chalk.black.bgBlack('\tPIAIC-Batch43 (Q1) \n')));
+
+let option;
+do{
+    let answers:any = await inquirer.prompt([ // prompt to take inputs
+        {
+            type: "list",
+            name: "calculation",
+            message: "Choose Operator:",
+            choices: ['Add', 'Subract', 'Multiply', 'Divide', 'Modulus']
+        },
+        {
+            type: "number",
+            name: "number1",
+            message: chalk.blue("Enter First Number:"),   
+        },
+        {
+            type: "number",
+            name: "number2",
+            message: chalk.blue("Enter Second Number:"),
+        },
+    ]);
+    // conditionals to check condition for Arithmatic Operation
+    switch(answers.calculation){
+    case 'Add':
+        console.log(chalk.yellow(`Addition Result:  ${answers.number1 + answers.number2}`));
+        break;
+    case 'Subract':
+        console.log(chalk.yellow(`Subraction Result:  ${answers.number1 - answers.number2}`));
+        break;
+    case 'Multiply':
+        console.log(chalk.yellow(`Multiplication Result:  ${answers.number1 * answers.number2}`));
+        break;
+    case 'Divide':
+        console.log(chalk.yellow(`Division Result:  ${answers.number1 / answers.number2}`));
+        break;
+    case 'Modulus':
+        console.log(chalk.yellow(`Modulus Result:  ${answers.number1 % answers.number2}`));
+        break;
+    }
+   
+    
+    // repeat the process to perform another operation
+    let choice = await inquirer.prompt([{
+        type: 'confirm',
+        name: "selection",
+        message: chalk.cyan("Perform another operation press Y for Yes and N for No: "),
+        default: false
+    }   
+    ]);
+    if(choice.selection){
+        option = 'y'
+    } else {
+        option = 'n'
+    }
+}while(option.toLowerCase() === "y") // check condition to repeat the process.
+ 
